15115472cbf4a545f24a31821c52156b0f902466
Reza Ranjkesh Shoorkaii <reza@Rezas-iMac-2.local>
2020-08-24 15:55:41 +0430

    localStrategy constructor

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,14 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import {Strategy} from 'passport-local'
-import { ModuleRef } from '@nestjs/core';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private moduleRef: ModuleRef){
-    super({
-      passReqToCallback: true
-    })
+  constructor(private readonly authService: AuthService) {
+    super();
   }
 
   async validate(username: string, password: string): Promise<any> {
